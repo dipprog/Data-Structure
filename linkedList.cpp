cef4de0c1d4c2eb2d41e509d940c553ca3153cca
Dip Kumar Das <dipkumardasdh@gmail.com>
2020-02-14 19:51:26 +0530

    add delete at nth position function

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string> // for getline() function
 using namespace std;
 
 class Node
@@ -8,7 +9,7 @@ private:
     Node *next;
 
 public:
-    Node *insert(Node *head, int x)
+    Node *insert(Node *head, int x) // insert element at the beginning
     {
         Node *temp = new Node();
         temp->data = x;
@@ -16,7 +17,7 @@ public:
         head = temp;
         return head;
     }
-    Node *insert(Node *head, int x, int pos)
+    Node *insert(Node *head, int x, int pos) // insert element at ith position
     {
         Node *temp1 = new Node();
         temp1->data = x;
@@ -34,6 +35,23 @@ public:
         }
         temp1->next = temp2->next;
         temp2->next = temp1;
+        return head;
+    }
+    Node *deletee(Node *head, int pos)
+    { // Delete nth node
+        Node *temp1 = head;
+        if (pos == 1)
+        {
+            head = temp1->next;
+            delete temp1;
+            return head;
+        }
+        for (int i = 0; i < pos - 2; i++)
+            temp1 = temp1->next;
+        // After for loop, temp1 points to (n-1)th node
+        Node *temp2 = temp1->next; // nth node
+        temp1->next = temp2->next; // (n+1)th node
+        delete temp2;
         return head;
     }
     void print(Node *head)
@@ -59,16 +77,28 @@ int main()
     head = N.insert(head, 1);
     head = N.insert(head, 10);
     N.print(head);
-    cout << "How many numbers?" << endl;
-    int size, input, pos;
-    cin >> size;
-    for (int i = 0; i < size; i++)
+    // cout << "How many numbers?" << endl;
+    // int size, input, pos;
+    // cin >> size;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout << "Enter the number " << endl;
+    //     cin >> input;
+    //     cout << "Enter Position " << endl;
+    //     cin >> pos;
+    //     head = N.insert(head, input, pos);
+    //     N.print(head);
+    // }
+    string pos;
+    int n;
+    while (true)
     {
-        cout << "Enter the number " << endl;
-        cin >> input;
-        cout << "Enter Position " << endl;
-        cin >> pos;
-        head = N.insert(head, input, pos);
+        cout << "Enter position to delete or type 'quit'" << endl;
+        getline(cin, pos);
+        if (pos.compare("quit") == 0)
+            break;
+        n = stoi(pos);
+        head = N.deletee(head, n);
         N.print(head);
     }
 
