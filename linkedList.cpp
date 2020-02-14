efc562103104d1a5cfd8c68b9ffbb42eef937f12
Dip Kumar Das <dipkumardasdh@gmail.com>
2020-02-14 18:52:25 +0530

    Second version - add insert at ith position

--- linkedList.cpp
+++ linkedList.cpp
@@ -16,7 +16,26 @@ public:
         head = temp;
         return head;
     }
-
+    Node *insert(Node *head, int x, int pos)
+    {
+        Node *temp1 = new Node();
+        temp1->data = x;
+        temp1->next = NULL;
+        if (pos == 1)
+        {
+            temp1->next = head;
+            head = temp1;
+            return head;
+        }
+        Node *temp2 = head;
+        for (int i = 0; i < pos - 2; i++) // pos-2 not pos-1 since initially pos = 1 (temp2 = head)
+        {
+            temp2 = temp2->next;
+        }
+        temp1->next = temp2->next;
+        temp2->next = temp1;
+        return head;
+    }
     void print(Node *head)
     {
         cout << "List is: " << endl;
@@ -33,15 +52,25 @@ int main()
 {
     Node N;
     Node *head = NULL; // empty list
+    head = N.insert(head, 2);
+    head = N.insert(head, 4);
+    head = N.insert(head, 5);
+    head = N.insert(head, 3);
+    head = N.insert(head, 1);
+    head = N.insert(head, 10);
+    N.print(head);
     cout << "How many numbers?" << endl;
-    int size, input;
+    int size, input, pos;
     cin >> size;
     for (int i = 0; i < size; i++)
     {
-        cout << "Enter the number" << endl;
+        cout << "Enter the number " << endl;
         cin >> input;
-        head = N.insert(head, input);
+        cout << "Enter Position " << endl;
+        cin >> pos;
+        head = N.insert(head, input, pos);
         N.print(head);
     }
+
     return 0;
 }
